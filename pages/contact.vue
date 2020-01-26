8e7b15768a7fde958c53710a4cdaf51ceb92f2f7
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-01-26 10:33:33 +0900

    make contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,23 @@
 <template>
   <section class="contact">
     <h1 class="contact__title">contact</h1>
+    <form method="POST" name="contact" class="contact__form" netlify>
+      <p class="contact__form__item">
+        <label for="name">お名前</label>
+        <input type="text" id="name" name="name" />
+      </p>
+      <p class="contact__form__item">
+        <label for="email">Eメール</label>
+        <input type="email" id="email" name="email" />
+      </p>
+      <p class="contact__form__item">
+        <label for="content">内容</label>
+        <textarea name="content" id="content" cols="30" rows="10"></textarea>
+      </p>
+      <p class="contact__form__item">
+        <button type="submit">送信</button>
+      </p>
+    </form>
   </section>
 </template>
 
@@ -15,6 +32,19 @@
     text-align: center;
     padding: 0 $side-space;
     @include title;
+  }
+  &__form {
+    width: 100%;
+    margin: 0 auto;
+    &__item {
+      display: flex;
+      flex-direction: column;
+      margin-bottom: $header-height/2;
+    }
+    &__item:last-child {
+      display: block;
+      text-align: center;
+    }
   }
 }
 </style>
