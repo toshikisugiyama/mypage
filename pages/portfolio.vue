e38896831359c99f5a8b18c380c7a759731a4929
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-02-24 16:12:31 +0900

    add todo app in portfolio

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -20,6 +20,7 @@
 <script>
 import name_game from '@/assets/images/name_game.svg'
 import count_down from '@/assets/images/count_down.svg'
+import todo_app from '@/assets/images/todo_app.svg'
 export default {
   data() {
     return {
@@ -27,6 +28,7 @@ export default {
       works: [
         {title: '名前をつけて早く呼ぶゲーム', src: name_game, url: 'https://toshikisugiyama-images.netlify.com/', description: 'Adobe Illustrator による自作のイラストを用いたゲーム。もともと JavaScript で動かしていたが、学習のために React , Vueで作り直した。'},
         {title: '発表オープニング用カウントダウン', src: count_down, url: 'https://toshikisugiyama-ns-countdown.netlify.com/', description: '発表会の司会進行で、オープニングの際に開始までのカウントダウンをスクリーンに映したくて作った。学習のために React を使用。最初に時間を設定すると、その時間までのカウントダウンができる。設定時間になり、画面をクリックすると、発表の注意事項に移る。時間設定の際の初期設定は、17時（発表会が大抵17時開始だった為）。'},
+        {title: 'ToDoアプリ', src: todo_app, url: 'http://obscure-falls-29065.herokuapp.com/', description: 'LaravelとVue.jsで作ったTodoアプリ。ログインログアウト機能、フォルダ追加機能、タスク追加、編集、削除機能を実装。'},
       ],
     }
   },
